9e7bdb00cea9f462bf11db0b66fed02464520b7e
Thiago Bottoni <bottonithiago@gmail.com>
2023-09-26 22:50:29 -0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # @thiagobottoni
    # Input:
    # p, principal amount
    # t, time period in years
